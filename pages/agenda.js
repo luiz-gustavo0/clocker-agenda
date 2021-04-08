0cadd0de793756e25d51db8e9e7a68377deee288
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-04-08 17:36:19 -0300

    feat: date header

--- pages/agenda.js
+++ pages/agenda.js
@@ -1,18 +1,71 @@
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import { Button } from '@chakra-ui/button';
-import { useAuth } from '../components';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useFetch } from '@refetty/react';
+import { addDays, subDays } from 'date-fns';
+
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
+import { Container, Button, Box, IconButton } from '@chakra-ui/react';
+import { formatDate, Logo, useAuth } from '../components';
+
+const getAgenda = ({ token, when }) =>
+  axios({
+    method: 'get',
+    url: '/api/agenda',
+    params: { when },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+const Header = ({ children }) => (
+  <Box display='flex' alignItems='center' justifyContent='space-between' py='4'>
+    {children}
+  </Box>
+);
 
 export default function Agenda() {
-  const [auth, { logout }] = useAuth();
   const router = useRouter();
+  const [auth, { logout }] = useAuth();
+
+  const [when, setWhen] = useState(() => new Date());
+  const [data, { loading, status, error }, fetch] = useFetch(getAgenda, {
+    lazy: true,
+  });
+
+  const addDay = () => setWhen((prevState) => addDays(prevState, 1));
+  const removeDay = () => setWhen((prevState) => subDays(prevState, 1));
+
   useEffect(() => {
     !auth.user && router.push('/');
   }, [auth.user]);
 
+  useEffect(() => {
+    fetch(when);
+  }, [when]);
+
   return (
-    <div>
-      <Button onClick={logout}>Sair</Button>
-    </div>
+    <Container>
+      <Header>
+        <Logo size={150} />
+        <Button onClick={logout}>Sair</Button>
+      </Header>
+
+      <Box py='8' display='flex' alignItems='center' justifyContent='center'>
+        <IconButton
+          bg='transparent'
+          icon={<ChevronLeftIcon />}
+          onClick={removeDay}
+        />
+        <Box flex='1' textAlign='center'>
+          {formatDate(when, 'PPPP')}
+        </Box>
+        <IconButton
+          bg='transparent'
+          icon={<ChevronRightIcon />}
+          onClick={addDay}
+        />
+      </Box>
+    </Container>
   );
 }
